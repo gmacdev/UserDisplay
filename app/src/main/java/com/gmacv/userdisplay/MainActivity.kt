724a3e8c46bcf597de174a4a6e57630b1ee5354a
Gulam <gmacdevs@gmail.com>
2023-07-17 22:35:38 +0530

    initial commit

---
+++ app/src/main/java/com/gmacv/userdisplay/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.gmacv.userdisplay
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gmacv.userdisplay.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAnchorView(R.id.fab)
+                .setAction("Action", null).show()
+        }
+    }
+}
